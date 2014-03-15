c447a342c3177302943a3d8d236615070689e007
Ilnar Salimzyan <ilnar@selimcan.org>
2014-03-15 20:44:51 +0000

    regression test scripts

---
+++ pending-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+C=2
+GREP='.'
+if [ $# -eq 1 ]
+then
+    C=$1
+    GREP='WORKS'
+fi
+
+sh wiki-tests.sh Pending tat rus update | grep -C $C "$GREP"
+
---
+++ regression-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+C=2
+GREP='.'
+if [ $# -eq 1 ]
+then
+    C=$1
+    GREP='WORKS'
+fi
+
+sh wiki-tests.sh Regression tat rus update | grep -C $C "$GREP"
