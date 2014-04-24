e4f54a4a7331eaf0d28053fd475878d183859fec
Ilnar Salimzyan <ilnar@selimcan.org>
2014-04-24 17:27:34 +0000

    first go of the testvoc scripts

--- testvoc/testvoc.sh
+++ testvoc/testvoc.sh
@@ -1,8 +1,14 @@
-#! /bin/bash
+#!/bin/bash
+
+# A wrapper sccript to run the testvoc.
+# Usage: [TMPDIR=...] ./testvoc.sh
 
 if [ -z $TMPDIR ]; then
 	TMPDIR="/tmp"
 fi
 
-echo "==Tatar->Russian===========================";
-bash inconsistency.sh tat-rus > $TMPDIR/tat-rus.testvoc; bash inconsistency-summary.sh $TMPDIR/tat-rus.testvoc tat-rus
+export TMPDIR
+
+echo "==Tatar->Russian==========================="
+bash inconsistency.sh tat-rus > $TMPDIR/tat-rus.testvoc
+bash inconsistency-summary.sh $TMPDIR/tat-rus.testvoc tat-rus
