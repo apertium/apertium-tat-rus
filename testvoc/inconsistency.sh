50a62491164971e3575af998a783502f9ad27461
Ilnar Salimzyan <ilnar@selimcan.org>
2014-05-02 12:04:46 +0000

    full testvoc scripts basically work now (see testvoc.sh for warnings), inconsistency-summary.sh has to be checked, since there is some odd stuff in the output

--- testvoc/inconsistency.sh
+++ testvoc/inconsistency.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-# This script expands the source language dictionary/transducer and passes it
-# through the translator.
+# This script expands the source language dictionary/transducer MONODIX and
+# passes the result of the expansion through the translator.
 # It is supposed to be invoked by ./testvoc.sh, and not run directly.
+
+if [ -z $TMPDIR ]; then
+	TMPDIR="/tmp"
+fi
 
 DIR=$1
 
-MONODIX=../.deps/tat-rus.automorf.trimmed
-BIDIXBIN=../tat-rus.autobil.bin
-T1X=../apertium-tat-rus.tat-rus.t1x; T1XBIN=../tat-rus.t1x.bin
-T2X=../apertium-tat-rus.tat-rus.t2x; T2XBIN=../tat-rus.t2x.bin
-T3X=../apertium-tat-rus.tat-rus.t3x; T3XBIN=../tat-rus.t3x.bin
-GENERATORBIN=../tat-rus.autogen.bin
+function expand_monodix {
+    hfst-fst2strings $MONODIX | sort -u | sed 's/:/%/g' | cut -f2 -d'%' | \
+    sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt
+}
 
 if [[ $DIR = "tat-rus" ]]; then
 
-hfst-fst2strings $MONODIX | sort -u |  sed 's/:/%/g' | cut -f1 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
-        apertium-pretransfer |
-        apertium-transfer $T1X $T1XBIN $BIDIXBIN |
-        apertium-transfer -n $T2X $T2XBIN |
-        apertium-transfer -n $T3X $T3XBIN | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc $GENERATORBIN > $TMPDIR/tmp_testvoc3.txt
-paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
+    MONODIX=../.deps/tat-rus.automorf.trimmed
+    BIDIXBIN=../tat-rus.autobil.bin
+    T1X=../apertium-tat-rus.tat-rus.t1x; T1XBIN=../tat-rus.t1x.bin
+    T2X=../apertium-tat-rus.tat-rus.t2x; T2XBIN=../tat-rus.t2x.bin
+    T3X=../apertium-tat-rus.tat-rus.t3x; T3XBIN=../tat-rus.t3x.bin
+    GENERATORBIN=../tat-rus.autogen.bin
+
+    expand_monodix |
+    apertium-pretransfer |
+    apertium-transfer $T1X $T1XBIN $BIDIXBIN |
+    apertium-interchunk $T2X $T2XBIN |
+    apertium-postchunk $T3X $T3XBIN | tee $TMPDIR/tmp_testvoc2.txt |
+    lt-proc -d $GENERATORBIN > $TMPDIR/tmp_testvoc3.txt
+    paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
 else
 	echo "./inconsistency.sh <direction>";
--- testvoc/testvoc.sh
+++ testvoc/testvoc.sh
@@ -9,6 +9,14 @@ fi
 
 export TMPDIR
 
+# With Tatar transducer being trimmed to the bilingual dictionary, it is
+# possible to run the full testvoc in a reasonable time (at least now in the
+# beginning, when the bidix is small) *under one condition* -- we do not include
+# (cyclic) digits stuff in the .deps/tat-rus.automorf.trimmed. In order to trim
+# them, we have to get rid from the following line in tat-rus.dix:
+# <e>       <re>[№%]?[0-9]+([.,][0-9]+)*[.,]*</re><p><l><s n="num"/></l><r><s n="num"/><s n="digit"/></r></p></e>
+# and then compile.
+
 echo "==Tatar->Russian==========================="
 bash inconsistency.sh tat-rus > $TMPDIR/tat-rus.testvoc
 bash inconsistency-summary.sh $TMPDIR/tat-rus.testvoc tat-rus
