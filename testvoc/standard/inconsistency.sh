4906e088cc699bf339c72327d10a1c506e74016c
Ilnar Salimzyan <ilnar@selimcan.org>
2014-05-14 14:27:16 +0000

    modify testvoc.sh to provide input for inconsistency.sh (so that we can use one inconsistency.sh script for different kinds of testvocs)

--- testvoc/standard/inconsistency.sh
+++ testvoc/standard/inconsistency.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
-# This script expands the source language dictionary/transducer MONODIX and
-# passes the result of the expansion through the translator.
-# It is supposed to be invoked by ./testvoc.sh, and not run directly.
+# Passes its input -- a list of lexical units -- through the translator
+# (transfer modules and target language generator).
+# Creates three text files in TMPDIR:
+#     1) INPUT, a list of lexical units taken
+#     2) TRANSFOUT, this list after passing transfer modules
+#     3) GENOUT, this list after TL generator.
+# Outputs "paste INPUT TRANSFOUT GENOUT"
+# Supposed to be invoked by ./testvoc.sh, and not run directly.
 
 if [ -z $TMPDIR ]; then
 	TMPDIR="/tmp"
 fi
 
-DIR=$1
+INPUT=$TMPDIR/testvoc_input.txt
+TRANSFOUT=$TMPDIR/testvoc_transfout.txt
+GENOUT=$TMPDIR/testvoc_genout.txt
 
-function expand_monodix {
-    hfst-fst2strings $MONODIX | sort -u | sed 's/:/%/g' | cut -f2 -d'%' | \
-    sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt
-}
+DIR=$1
 
 if [[ $DIR = "tat-rus" ]]; then
 
-    MONODIX=../../.deps/tat-rus.automorf.trimmed
     BIDIXBIN=../../tat-rus.autobil.bin
     T1X=../../apertium-tat-rus.tat-rus.t1x; T1XBIN=../../tat-rus.t1x.bin
     T2X=../../apertium-tat-rus.tat-rus.t2x; T2XBIN=../../tat-rus.t2x.bin
     T3X=../../apertium-tat-rus.tat-rus.t3x; T3XBIN=../../tat-rus.t3x.bin
     GENERATORBIN=../../tat-rus.autogen.bin
 
-    expand_monodix |
+    tee $INPUT |
     apertium-pretransfer |
     apertium-transfer $T1X $T1XBIN $BIDIXBIN |
     apertium-interchunk $T2X $T2XBIN |
-    apertium-postchunk $T3X $T3XBIN | tee $TMPDIR/tmp_testvoc2.txt |
-    lt-proc -d $GENERATORBIN > $TMPDIR/tmp_testvoc3.txt
-    paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt \
-	$TMPDIR/tmp_testvoc3.txt | 
-    sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
+    apertium-postchunk $T3X $T3XBIN | tee $TRANSFOUT |
+    lt-proc -d $GENERATORBIN > $GENOUT
+    paste -d _ $INPUT $TRANSFOUT $GENOUT | 
+    sed 's/\^.<sent>\$//g' | sed 's/_/   -->  /g'
 
 else
 	echo "./inconsistency.sh <direction>";
--- testvoc/standard/testvoc.sh
+++ testvoc/standard/testvoc.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# A wrapper script to run the standard (=full) testvoc.
-# Usage: [TMPDIR=/path/to/tmpdir] ./testvoc.sh
+# A script to run the standard (=full) testvoc.
+#
 # Assumes the pair is compiled.
-# Produces 'testvoc-summary.tat-rus.txt' file.
+# Expands the source language dictionary/transducer MONODIX and passes the
+# result of the expansion through the translator (=inconsistency.sh script).
+# Produces 'testvoc-summary.tat-rus.txt' file using the inconsistency-summary.sh.
+#
+# Usage: [TMPDIR=/path/to/tmpdir] ./testvoc.sh
 
 if [ -z $TMPDIR ]; then
 	TMPDIR="/tmp"
@@ -23,6 +27,19 @@ sed -i 's_^<e>       <re>\[№.*$_<!--&-->_' apertium-tat-rus.tat-rus.dix
 make
 cd testvoc/standard/
 
+function expand_monodix {
+    hfst-fst2strings $MONODIX | sort -u | sed 's/:/%/g' | cut -f2 -d'%' | \
+    sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g'
+}
+
+#-------------------------------------------------------------------------------
+# Tatar->Russian testvoc
+#-------------------------------------------------------------------------------
+
+MONODIX=../../.deps/tat-rus.automorf.trimmed
+
 echo "==Tatar->Russian==========================="
+
+expand_monodix |
 bash inconsistency.sh tat-rus > $TMPDIR/tat-rus.testvoc
 bash inconsistency-summary.sh $TMPDIR/tat-rus.testvoc tat-rus
