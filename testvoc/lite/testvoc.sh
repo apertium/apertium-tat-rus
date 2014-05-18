4ada512123f6f6fee44ac00f0fa426350874d325
Ilnar Salimzyan <ilnar@selimcan.org>
2014-05-18 13:13:30 +0000

    make testvoc.sh uncompress txt.gz files from apertium-tat/tests/morphotactics first

--- testvoc/lite/testvoc.sh
+++ testvoc/lite/testvoc.sh
@@ -3,10 +3,11 @@
 # A script to run the "lite" ("one-word-per-each-paradigm-") testvoc.
 #
 # Assumes the pair is compiled.
-# Extracts lexical units from text files in languages/apertium-tat/tests/
-# morphotactics/ and passes them through the translator (=INCONSISTENCY script).
-# Generates a one-line stats about each file using the INCONSISTENCY-SUMMARY
-# script and produces 'testvoc-summary.tat-rus.txt' file.
+# Extracts lexical units from compressed text files in languages/apertium-tat/
+# tests/morphotactics/ and passes them through the translator (=INCONSISTENCY
+# script).
+# TODO: Generate a one-line stats about each file using the
+# INCONSISTENCY-SUMMARY script and produce 'testvoc-summary.tat-rus.txt' file.
 #
 # Usage: [TMPDIR=/path/to/tmpdir] ./testvoc.sh
 
@@ -26,14 +27,16 @@ function extract_lexical_units {
 
 echo "==Tatar->Russian==========================="
 
-PARDEF_FILES=../../../../languages/apertium-tat/tests/morphotactics/*.txt
+PARDEF_FILES=../../../../languages/apertium-tat/tests/morphotactics/*.txt.gz
 OUT=testvoc-summary.tat-rus.txt
 
 echo "" > $OUT
 date >> $OUT
-$ECHOE  "===============================================" >> "$OUT"
-$ECHOE  "POS\tTotal\tClean\tWith @\tWith #\tClean %" >> "$OUT"
+echo "===============================================" >> "$OUT"
+echo "POS\tTotal\tClean\tWith @\tWith #\tClean %" >> "$OUT"
+
 for file in $PARDEF_FILES; do
-    cat $file | extract_lexical_units |
+    zcat $file | extract_lexical_units |
     $INCONSISTENCY tat-rus
 done
+
