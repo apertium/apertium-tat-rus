9ad684cbe6471c86cc48f39b8a871b10efc97a99
Ilnar Salimzyan <ilnar@selimcan.org>
2014-05-24 09:48:15 +0000

    minor fix in the purpose statement

--- qa.sh
+++ qa.sh
@@ -1,14 +1,6 @@
-# Takes the test name in /tests as an argument, an additional argument
-# if the test requires it (e.g. corpus name) and runs the test.
+# Takes the basename of the test scrpt in /tests as an argument
+# and runs the test.
 
 #!/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    testToRun=tat-rus.test
-  else
-    testToRun=$1.test
-fi
-
-bash "tests/$testToRun"
-
+bash "tests/$1.test"
